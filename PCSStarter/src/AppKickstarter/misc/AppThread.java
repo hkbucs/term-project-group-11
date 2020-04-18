98d0f9610d84f301bda60f0e50bc41cab0ad660d
Ryan <15251497@life.hkbu.edu.hk>
2020-04-19 07:41:26 +0800

    20200419 - Reformat the code.

--- PCSStarter/src/AppKickstarter/misc/AppThread.java
+++ PCSStarter/src/AppKickstarter/misc/AppThread.java
@@ -1,6 +1,7 @@
 package AppKickstarter.misc;
 
 import AppKickstarter.AppKickstarter;
+
 import java.util.logging.Logger;
 
 
@@ -14,35 +15,45 @@ public abstract class AppThread implements Runnable {
 
     //------------------------------------------------------------
     // AppThread
+
     /**
      * Constructor for an appThread
-     * @param id name of the appThread
+     *
+     * @param id             name of the appThread
      * @param appKickstarter a reference to our AppKickstarter
      */
     public AppThread(String id, AppKickstarter appKickstarter) {
-	this.id = id;
-	this.appKickstarter = appKickstarter;
-	log = appKickstarter.getLogger();
-	mbox = new MBox(id, log);
-	appKickstarter.regThread(this);
-	log.fine(id + ": created!");
+        this.id = id;
+        this.appKickstarter = appKickstarter;
+        log = appKickstarter.getLogger();
+        mbox = new MBox(id, log);
+        appKickstarter.regThread(this);
+        log.fine(id + ": created!");
     } // AppThread
 
 
     //------------------------------------------------------------
     // getMBox
+
     /**
      * Get the mbox of this appThread
+     *
      * @return the mbox of this appThread
      */
-    public MBox getMBox() { return mbox; }
+    public MBox getMBox() {
+        return mbox;
+    }
 
 
     //------------------------------------------------------------
     // getID
+
     /**
      * Get the id of this appThread
+     *
      * @return the id of this appThread
      */
-    public String getID() { return id; }
+    public String getID() {
+        return id;
+    }
 } // AppThread
--- PCSStarter/src/PCS/GateHandler/Emulator/GateEmulator.java
+++ PCSStarter/src/PCS/GateHandler/Emulator/GateEmulator.java
@@ -18,14 +18,14 @@ import javafx.stage.WindowEvent;
 //======================================================================
 // GateEmulator
 public class GateEmulator extends GateHandler {
-    private Stage myStage;
-    private GateEmulatorController gateEmulatorController;
     private final PCSStarter pcsStarter;
     private final String id;
     private final int gateOpenTime;
     private final int gateCloseTime;
     private final int GateOpenTimerID = 1;
     private final int GateCloseTimerID = 2;
+    private Stage myStage;
+    private GateEmulatorController gateEmulatorController;
     private boolean autoOpen;
     private boolean autoClose;
     private boolean autoPoll;
@@ -34,37 +34,37 @@ public class GateEmulator extends GateHandler {
     //------------------------------------------------------------
     // GateEmulator
     public GateEmulator(String id, PCSStarter pcsStarter) {
-	super(id, pcsStarter);
-	this.pcsStarter = pcsStarter;
-	this.id = id + "Emulator";
-	this.gateOpenTime = Integer.parseInt(this.pcsStarter.getProperty("Gate.GateOpenTime"));
-	this.gateCloseTime = Integer.parseInt(this.pcsStarter.getProperty("Gate.GateCloseTime"));
-	this.autoOpen = true;
-	this.autoClose = true;
-	this.autoPoll = true;
+        super(id, pcsStarter);
+        this.pcsStarter = pcsStarter;
+        this.id = id + "Emulator";
+        this.gateOpenTime = Integer.parseInt(this.pcsStarter.getProperty("Gate.GateOpenTime"));
+        this.gateCloseTime = Integer.parseInt(this.pcsStarter.getProperty("Gate.GateCloseTime"));
+        this.autoOpen = true;
+        this.autoClose = true;
+        this.autoPoll = true;
     } // GateEmulator
 
 
     //------------------------------------------------------------
     // start
     public void start() throws Exception {
-	Parent root;
-	myStage = new Stage();
-	FXMLLoader loader = new FXMLLoader();
-	String fxmlName = "GateEmulator.fxml";
-	loader.setLocation(GateEmulator.class.getResource(fxmlName));
-	root = loader.load();
-	gateEmulatorController = (GateEmulatorController) loader.getController();
-	gateEmulatorController.initialize(id, pcsStarter, log, this);
-	myStage.initStyle(StageStyle.DECORATED);
-	myStage.setScene(new Scene(root, 420, 470));
-	myStage.setTitle("Gate Emulator");
-	myStage.setResizable(false);
-	myStage.setOnCloseRequest((WindowEvent event) -> {
-	    pcsStarter.stopApp();
-	    Platform.exit();
-	});
-	myStage.show();
+        Parent root;
+        myStage = new Stage();
+        FXMLLoader loader = new FXMLLoader();
+        String fxmlName = "GateEmulator.fxml";
+        loader.setLocation(GateEmulator.class.getResource(fxmlName));
+        root = loader.load();
+        gateEmulatorController = loader.getController();
+        gateEmulatorController.initialize(id, pcsStarter, log, this);
+        myStage.initStyle(StageStyle.DECORATED);
+        myStage.setScene(new Scene(root, 420, 470));
+        myStage.setTitle("Gate Emulator");
+        myStage.setResizable(false);
+        myStage.setOnCloseRequest((WindowEvent event) -> {
+            pcsStarter.stopApp();
+            Platform.exit();
+        });
+        myStage.show();
     } // GateEmulator
 
 
@@ -73,27 +73,27 @@ public class GateEmulator extends GateHandler {
     protected final boolean processMsg(Msg msg) {
         boolean quit = false;
 
-	switch (msg.getType()) {
-	    case TimesUp:
-	        handleTimesUp(msg);
-	        break;
+        switch (msg.getType()) {
+            case TimesUp:
+                handleTimesUp(msg);
+                break;
 
-	    case GateEmulatorAutoOpenToggle:
-		handleGateEmulatorAutoOpenToggle();
-		break;
+            case GateEmulatorAutoOpenToggle:
+                handleGateEmulatorAutoOpenToggle();
+                break;
 
-	    case GateEmulatorAutoCloseToggle:
-		handleGateEmulatorAutoCloseToggle();
-		break;
+            case GateEmulatorAutoCloseToggle:
+                handleGateEmulatorAutoCloseToggle();
+                break;
 
-	    case GateEmulatorAutoPollToggle:
-		handleGateEmulatorAutoPollToggle();
-	        break;
+            case GateEmulatorAutoPollToggle:
+                handleGateEmulatorAutoPollToggle();
+                break;
 
-	    default:
-		quit = super.processMsg(msg);
-	}
-	return quit;
+            default:
+                quit = super.processMsg(msg);
+        }
+        return quit;
     } // processMsg
 
 
@@ -101,11 +101,11 @@ public class GateEmulator extends GateHandler {
     // sendGateOpenSignal
     @Override
     protected void sendGateOpenSignal() {
-	logFine("Gate open signal received.  [autoOpen is " + (autoOpen ? "on]" : "off]"));
+        logFine("Gate open signal received.  [autoOpen is " + (autoOpen ? "on]" : "off]"));
         if (autoOpen) {
-	    logFine("Gate open timer started.");
-	    Timer.setTimer(id, mbox, gateOpenTime, GateOpenTimerID);
-	}
+            logFine("Gate open timer started.");
+            Timer.setTimer(id, mbox, gateOpenTime, GateOpenTimerID);
+        }
     } // sendGateOpenSignal
 
 
@@ -113,11 +113,11 @@ public class GateEmulator extends GateHandler {
     // sendGateCloseSignal
     @Override
     protected void sendGateCloseSignal() {
-	logFine("Gate close signal received.  [autoClose is " + (autoClose ? "on]" : "off]"));
-	if (autoClose) {
-	    logFine("Gate close timer started.");
-	    Timer.setTimer(id, mbox, gateCloseTime, GateCloseTimerID);
-	}
+        logFine("Gate close signal received.  [autoClose is " + (autoClose ? "on]" : "off]"));
+        if (autoClose) {
+            logFine("Gate close timer started.");
+            Timer.setTimer(id, mbox, gateCloseTime, GateCloseTimerID);
+        }
     } // sendGateCloseSignal
 
 
@@ -125,63 +125,63 @@ public class GateEmulator extends GateHandler {
     // sendPollReq
     @Override
     protected void sendPollReq() {
-	logFine("Poll request received.  [autoPoll is " + (autoPoll ? "on]" : "off]"));
-	if (autoPoll) {
-	    logFine("Send poll ack.");
-	    mbox.send(new Msg(id, mbox, Msg.Type.PollAck, ""));
-	}
+        logFine("Poll request received.  [autoPoll is " + (autoPoll ? "on]" : "off]"));
+        if (autoPoll) {
+            logFine("Send poll ack.");
+            mbox.send(new Msg(id, mbox, Msg.Type.PollAck, ""));
+        }
     } // sendPollReq
 
 
     //------------------------------------------------------------
     // handleTimesUp
     public final void handleTimesUp(Msg msg) {
-	logFine("Times up received.");
+        logFine("Times up received.");
 
-	switch (Timer.getTimesUpMsgTimerId(msg)) {
-	    case GateOpenTimerID:
-		logFine("Gate open timer is up.  [autoOpen is " + (autoOpen ? "on]" : "off]"));
-	        if (autoOpen) {
-		    logFine("Send gate open reply");
-		    mbox.send(new Msg(id, mbox, Msg.Type.GateOpenReply, "Gate Open Reply"));
-		} else {
-	            // autoOpen is off.  just ignore timeout msg
-		    logFine("Auto open is off.  Timer ignored.");
-		}
-		break;
+        switch (Timer.getTimesUpMsgTimerId(msg)) {
+            case GateOpenTimerID:
+                logFine("Gate open timer is up.  [autoOpen is " + (autoOpen ? "on]" : "off]"));
+                if (autoOpen) {
+                    logFine("Send gate open reply");
+                    mbox.send(new Msg(id, mbox, Msg.Type.GateOpenReply, "Gate Open Reply"));
+                } else {
+                    // autoOpen is off.  just ignore timeout msg
+                    logFine("Auto open is off.  Timer ignored.");
+                }
+                break;
 
-	    case GateCloseTimerID:
-		logFine("Gate close timer is up.  [autoClose is " + (autoClose ? "on]" : "off]"));
-		if (autoClose) {
-		    logFine("Send gate close reply");
-		    mbox.send(new Msg(id, mbox, Msg.Type.GateCloseReply, "Gate Close Reply"));
-		} else {
-		    // autoClose is off.  just ignore timeout msg
-		    logFine("Auto close is off.  Timer ignored.");
-		}
-		break;
+            case GateCloseTimerID:
+                logFine("Gate close timer is up.  [autoClose is " + (autoClose ? "on]" : "off]"));
+                if (autoClose) {
+                    logFine("Send gate close reply");
+                    mbox.send(new Msg(id, mbox, Msg.Type.GateCloseReply, "Gate Close Reply"));
+                } else {
+                    // autoClose is off.  just ignore timeout msg
+                    logFine("Auto close is off.  Timer ignored.");
+                }
+                break;
 
-	    default:
-		logSevere("Unknown timer id!!");
-	}
+            default:
+                logSevere("Unknown timer id!!");
+        }
     } // handleTimesUp
 
 
     //------------------------------------------------------------
     // handleGateEmulatorAutoOpenToggle
     public final boolean handleGateEmulatorAutoOpenToggle() {
-	autoOpen = !autoOpen;
-	logFine("Auto open change: " + (autoOpen ? "off --> on" : "on --> off"));
-	return autoOpen;
+        autoOpen = !autoOpen;
+        logFine("Auto open change: " + (autoOpen ? "off --> on" : "on --> off"));
+        return autoOpen;
     } // handleGateEmulatorAutoOpenToggle
 
 
     //------------------------------------------------------------
     // handleGateEmulatorAutoCloseToggle
     public final boolean handleGateEmulatorAutoCloseToggle() {
-	autoClose = !autoClose;
-	logFine("Auto close change: " + (autoClose ? "off --> on" : "on --> off"));
-	return autoClose;
+        autoClose = !autoClose;
+        logFine("Auto close change: " + (autoClose ? "off --> on" : "on --> off"));
+        return autoClose;
     } // handleGateEmulatorAutoCloseToggle
 
 
@@ -189,7 +189,7 @@ public class GateEmulator extends GateHandler {
     // handleGateEmulatorAutoPollToggle:
     public final boolean handleGateEmulatorAutoPollToggle() {
         autoPoll = !autoPoll;
-	logFine("Auto poll change: " + (autoPoll ? "off --> on" : "on --> off"));
+        logFine("Auto poll change: " + (autoPoll ? "off --> on" : "on --> off"));
         return autoPoll;
     } // handleGateEmulatorAutoPollToggle
 
@@ -197,31 +197,31 @@ public class GateEmulator extends GateHandler {
     //------------------------------------------------------------
     // logFine
     private final void logFine(String logMsg) {
-	gateEmulatorController.appendTextArea("[FINE]: " + logMsg);
-	log.fine(id + ": " + logMsg);
+        gateEmulatorController.appendTextArea("[FINE]: " + logMsg);
+        log.fine(id + ": " + logMsg);
     } // logFine
 
 
     //------------------------------------------------------------
     // logInfo
     private final void logInfo(String logMsg) {
-	gateEmulatorController.appendTextArea("[INFO]: " + logMsg);
-	log.info(id + ": " + logMsg);
+        gateEmulatorController.appendTextArea("[INFO]: " + logMsg);
+        log.info(id + ": " + logMsg);
     } // logInfo
 
 
     //------------------------------------------------------------
     // logWarning
     private final void logWarning(String logMsg) {
-	gateEmulatorController.appendTextArea("[WARNING]: " + logMsg);
-	log.warning(id + ": " + logMsg);
+        gateEmulatorController.appendTextArea("[WARNING]: " + logMsg);
+        log.warning(id + ": " + logMsg);
     } // logWarning
 
 
     //------------------------------------------------------------
     // logSevere
     private final void logSevere(String logMsg) {
-	gateEmulatorController.appendTextArea("[SEVERE]: " + logMsg);
-	log.severe(id + ": " + logMsg);
+        gateEmulatorController.appendTextArea("[SEVERE]: " + logMsg);
+        log.severe(id + ": " + logMsg);
     } // logSevere
 } // GateEmulator
